dee696f135f98e0fc3b832034fb5e364694d3b30
Tiago Gonçalves Vieira <tiagosistematizando@gmail.com>
2022-08-08 21:39:40 -0300

    Commit inicial

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# EnviandoObjetosRedeJava
+# Enviando objetos pela rede via Java
+
+Gerando o Servidor:
+```
+  mvn install -D my.mainClass=br.com.enviorede.Server
+```
+
+Gerando o Cliente:
+```
+mvn install -D my.mainClass=br.com.enviorede.Cliente
+```
+
+
+
---
+++ src/main/java/br/com/enviorede/Cliente.java
@@ -0,0 +1,33 @@
+package br.com.enviorede;
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+
+public class Cliente {
+
+	public static void main(String[] args) throws Exception {
+		Socket socket = new Socket("127.0.0.1", 3333);
+		
+		OutputStream saida = socket.getOutputStream();
+		ObjectOutput objectOutput = new ObjectOutputStream(saida);
+		
+		Pedido pedido = new Pedido();
+		pedido.setCodigo(2L);
+		pedido.setDescricao("Pasta de dente");
+		pedido.setQuantidade(2);
+		
+		objectOutput.writeObject(pedido);
+		
+		InputStream entrada = socket.getInputStream();
+		DataInputStream dataInput = new DataInputStream(entrada);
+		System.out.println("Recebeu do servidor: " + dataInput.readUTF());
+		
+		socket.close();
+		
+	}
+	
+}
